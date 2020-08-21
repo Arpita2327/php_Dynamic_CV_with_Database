cdb50259cb191f0710e9f6b7e05dbc082a49ec10
Arpita2327 <47477257+Arpita2327@users.noreply.github.com>
2020-08-21 20:39:14 +0600

    Create config.php

---
+++ config.php
@@ -0,0 +1,12 @@
+
+
+<?php
+
+$databaseHost = 'localhost';
+$databaseName = 'dynamic-cv';
+$databaseUsername = 'Arpita';
+$databasePassword = 'Web.1224';
+
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+ 
+?>
